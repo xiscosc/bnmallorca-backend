aa51f0eaaec1251c68b86f1195bfbd65bbb54887
Xisco Sastre Cabot <xiscosastre@gmail.com>
2026-02-19 11:01:15 +0100

    Refactor get-schedule.lambda.ts to remove unused event parameter and imports

--- src/function/schedule/get-schedule.lambda.ts
+++ src/function/schedule/get-schedule.lambda.ts
@@ -1,18 +1,11 @@
-import type {
-  APIGatewayEvent,
-  APIGatewayProxyEventV2,
-  APIGatewayProxyResultV2,
-  ProxyResult,
-} from 'aws-lambda';
+import type { APIGatewayProxyResultV2, ProxyResult } from 'aws-lambda';
 import { extractErrorMessage } from '../../helpers/error.helper';
 import { internalServerError, ok } from '../../helpers/lambda.helper';
 import { log } from '../../helpers/logger';
 import { ScheduleService } from '../../service/schedule.service';
 import type { ScheduleResponse } from '../../types/components';
 
-export async function handler(
-  event: APIGatewayEvent | APIGatewayProxyEventV2,
-): Promise<ProxyResult | APIGatewayProxyResultV2> {
+export async function handler(): Promise<ProxyResult | APIGatewayProxyResultV2> {
   try {
     const scheduleService = new ScheduleService();
     const days = await scheduleService.getSchedule();
